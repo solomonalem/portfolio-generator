316dab860413e24209505d0e1c23277e320e27d0
Solomon <solomonmeresa8@gmail.com>
2020-09-16 07:38:57 -0700

    adds asking questions and capturing the answer

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require("inquirer");
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
 
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
+// const profileDataArgs = process.argv.slice(2);
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile("./index.html", generatePage(name, github), (err) => {
+//   if (err) throw new Error(err);
+
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
